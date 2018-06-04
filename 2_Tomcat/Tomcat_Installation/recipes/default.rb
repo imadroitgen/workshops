58805609504affb1716b8e0c5daef332cb4b5c4a
imadroitgen <37382338+imadroitgen@users.noreply.github.com>
2018-06-04 05:13:15 -0400

    Create Users and Group for the Tomcat Installation

--- 2_Tomcat/Tomcat_Installation/recipes/default.rb
+++ 2_Tomcat/Tomcat_Installation/recipes/default.rb
@@ -15,3 +15,21 @@ package 'Install Java 1.7' do
   package_name 'java-1.7.0-openjdk-devel'
   action :install
 end
+
+# Instruction 2 : Create a user for Tomcat
+
+# Solution to Instruction 2
+# Step 1 : Create a user as per the instructions.md. Use the user resource of Chef (Chef Reference -> 
+user 'tomcat' do
+  uid 'tomcat'
+  home '/opt/tomcat'
+  shell '/bin/nologin'
+  action :create
+end
+
+# Step 2 : Create a group as per the instructions.md and add the user tomcat to the group. Use the group resource of Chef (Chef Reference -> https://docs.chef.io/resource_group.html)
+
+group 'tomcat' do
+  action :create
+  members 'tomcat'
+end  
