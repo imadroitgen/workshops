02d5141db08c63920ecb011341e81cad9d0b6294
Aamer Syed <imadroitgen@gmail.com>
2018-06-05 05:55:02 +0000

    Bonus Task - Writing Integration Tests for Inspec

--- 1_MongoDB/MongoDB_Installation/test/integration/default/default_test.rb
+++ 1_MongoDB/MongoDB_Installation/test/integration/default/default_test.rb
@@ -5,14 +5,26 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# Test 1 : Check if the mongodb-org-3.6.repo file is available with the relevant permissions
+
+describe file ('/etc/yum.repos.d/mongodb-org-3.6.repo') do
+ it { should exist }
+ its ('owner') { should eq 'root' }
+ its ('group') { should eq 'root' } 
+end 
+
+# Test 2 : Check the package mongodb-org is installed
+describe package('mongodb-org') do
+ it { should be_installed } 
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# Test 3 : Check the service mongod is enabled and running
+describe service('mongod') do
+ it { should be_enabled }
+ it { should be_running }
+end
+
+# Test 4 : Check that you are able to logon to mongodb using mongo
+describe command('mongo "quit()"') do
+  its('stdout') { should match "MongoDB server version: 3.6.5\n" }
 end
